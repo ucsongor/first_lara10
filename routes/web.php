83aa93f86d652b1858cdaf6f550e3cbd25cee0ca
U Csongor <ujvarosi.csongor981225@gmail.com>
2023-09-29 16:09:27 +0200

    admin panel, admin login with middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
+    Route::match(['get', 'host'], 'login', 'AdminController@login');
+    Route::group(['middleware'=>['admin']], function(){
+        Route::get('dashboard', 'AdminController@dashboard');
+    });
+});
+
+require __DIR__.'/auth.php';
